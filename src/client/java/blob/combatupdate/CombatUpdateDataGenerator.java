616459762b58ed86958dbe145bc2dabe0cededfa
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-01-26 15:43:57 +0100

    aba

---
+++ src/client/java/blob/combatupdate/CombatUpdateDataGenerator.java
@@ -0,0 +1,11 @@
+package blob.combatupdate;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CombatUpdateDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/blob/combatupdate/TestItem.java
@@ -0,0 +1,31 @@
+package blob.combatupdate;
+
+import java.util.function.Function;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+
+public class TestItem {
+	public static <GenericItem extends Item> GenericItem register(String name, Function<Item.Properties, GenericItem> itemFactory, Item.Properties settings) {
+		// Create the item key.
+		ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(CombatUpdate.MOD_ID, name));
+
+		// Create the item instance.
+		GenericItem item = itemFactory.apply(settings.setId(itemKey));
+
+		// Register the item.
+		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+		return item;
+	}
+	public static final Item TEST_ITEM1 = register("test1", Item::new, new Item.Properties());
+	public static void initialize() {
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register((itemGroup) -> itemGroup.accept(TestItem.TEST_ITEM1));
+	}
+}
