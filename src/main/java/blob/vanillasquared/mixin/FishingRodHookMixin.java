1143a755605e3dfb9a7a85b61399fe036b8b170f
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-01-30 18:16:48 +0100

    moar run files

--- src/main/java/blob/vanillasquared/mixin/FishingRodHookMixin.java
+++ src/main/java/blob/vanillasquared/mixin/FishingRodHookMixin.java
@@ -1,5 +1,6 @@
 package blob.vanillasquared.mixin;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
@@ -7,20 +8,24 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.FishingRodItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.SimpleExplosionDamageCalculator;
 import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,6 +34,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Debug(export = true)
 @Mixin(FishingHook.class)
@@ -40,42 +47,52 @@ public abstract class FishingRodHookMixin extends Projectile {
     @Unique // Damage type
     private static final ResourceKey<DamageType> FISHED = ResourceKey.create(Registries.DAMAGE_TYPE, Identifier.fromNamespaceAndPath("vanillasquared", "fished"));
 
-    // iInject starts
+    // @Inject starts
 
     @Inject(at = @At("TAIL"), method = "onHitEntity")
     private void init(EntityHitResult hit, CallbackInfo ci) {
         if (!(this.level() instanceof ServerLevel serverLevel)) return;
         if (!(this.getOwner() instanceof Player player)) return;
 
-        ItemStack weapon = player.getMainHandItem().isEmpty() ? player.getOffhandItem() : player.getMainHandItem();
+        ItemStack main = player.getMainHandItem();
+        ItemStack off  = player.getOffhandItem();
+        ItemStack weapon = main.getItem() instanceof FishingRodItem ? off : main;
 
         Entity target = hit.getEntity();
-        Entity owner = this.getOwner();
-        DamageSource source = serverLevel.damageSources().source(FISHED, this, owner);
+        if (!(target instanceof LivingEntity living)) return;
+
+        DamageSource source = serverLevel.damageSources().playerAttack(player);
 
         float damage = 0.5F;
+
         var enchants = serverLevel.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
 
-        int fire = EnchantmentHelper.getItemEnchantmentLevel(enchants.getOrThrow(Enchantments.FIRE_ASPECT), weapon);
+        int fire  = EnchantmentHelper.getItemEnchantmentLevel(enchants.getOrThrow(Enchantments.FIRE_ASPECT), weapon);
         int smite = EnchantmentHelper.getItemEnchantmentLevel(enchants.getOrThrow(Enchantments.SMITE), weapon);
-        int bane = EnchantmentHelper.getItemEnchantmentLevel(enchants.getOrThrow(Enchantments.BANE_OF_ARTHROPODS), weapon);
-        if (bane > 0 && target instanceof LivingEntity living && living.getType().is(EntityTypeTags.UNDEAD)) {
+        int bane  = EnchantmentHelper.getItemEnchantmentLevel(enchants.getOrThrow(Enchantments.BANE_OF_ARTHROPODS), weapon);
+
+        // <- Smite ->
+        if (smite > 0 && living.getType().is(EntityTypeTags.UNDEAD)) {
+            damage += smite * 2.5F;
+        }
+
+        // <- Bane of Arthropods ->
+        if (bane > 0 && living.getType().is(EntityTypeTags.ARTHROPOD)) {
             damage += bane * 2.5F;
 
             int duration = 20 + serverLevel.random.nextInt(10 * bane);
-            living.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, duration, 3));
+            living.addEffect(new MobEffectInstance(MobEffects.SLOWNESS, duration, 3));
         }
-        if (smite > 0 && target instanceof LivingEntity living && living.getType().is(EntityTypeTags.UNDEAD)) {
 
-            damage += smite * 2.5F;
-        }
+        // <- Fire Aspect ->
         if (fire > 0) {
-            target.igniteForSeconds(4 * fire);
+            living.igniteForSeconds(4 * fire);
+
             serverLevel.playSound(
                     null,
-                    target.getX(),
-                    target.getY(),
-                    target.getZ(),
+                    living.getX(),
+                    living.getY(),
+                    living.getZ(),
                     SoundEvents.FIRECHARGE_USE,
                     SoundSource.PLAYERS,
                     0.7F,
@@ -83,13 +100,66 @@ public abstract class FishingRodHookMixin extends Projectile {
             );
         }
 
-        target.hurtServer(serverLevel, source, damage);
-        EnchantmentHelper.doPostAttackEffects(serverLevel, target, source);
+        // <-- Damage Player -->
+        living.hurtServer(serverLevel, source, damage);
 
-        if (target instanceof Player playerTarget && weapon.getItem() instanceof AxeItem && playerTarget.isBlocking())  {
+        // <- Knockback ->
+        float kb = EnchantmentHelper.modifyKnockback(serverLevel, weapon, living, source, 0.4F);
+
+        if (kb > 0) {
+            double yaw = Math.toRadians(player.getYRot());
+
+            living.push(
+                    -Math.sin(yaw) * kb,
+                    0.1,
+                    Math.cos(yaw) * kb
+            );
+
+            living.hurtMarked = true;
+        }
+
+        // <- Wind Burst ->
+        int wind = EnchantmentHelper.getItemEnchantmentLevel(
+                enchants.getOrThrow(Enchantments.WIND_BURST), weapon);
+
+        if (wind > 0) {
+            serverLevel.explode(
+                    null,                  // attribute to player
+                    null,                    // no damage source
+                    new SimpleExplosionDamageCalculator(
+                            false,           // no block damage
+                            false,           // no entity damage
+                            Optional.of(1.0F * wind), // knockback multiplier
+                            Optional.empty()
+                    ),
+                    player.getX(),
+                    player.getY(),
+                    player.getZ(),
+                    1.2F + 0.35F * wind,     // radius scaling (copied from enchant)
+                    false,
+                    Level.ExplosionInteraction.TRIGGER,
+                    ParticleTypes.GUST_EMITTER_SMALL,
+                    ParticleTypes.GUST_EMITTER_LARGE,
+                    WeightedList.of(),
+                    SoundEvents.WIND_CHARGE_BURST
+            );
+        }
+
+        // <- Shield Break ->
+        if (living instanceof Player playerTarget
+                && weapon.getItem() instanceof AxeItem
+                && playerTarget.isBlocking()) {
+
             ItemStack using = playerTarget.getUseItem();
+
             if (!using.isEmpty() && using.is(Items.SHIELD)) {
+
+                EquipmentSlot slot = playerTarget.getUsedItemHand().asEquipmentSlot();
+
+                // damage shield exactly like vanilla
                 playerTarget.getCooldowns().addCooldown(using, 100);
+                using.hurtAndBreak(5, playerTarget, slot);
+
                 serverLevel.playSound(
                         null,
                         playerTarget.getX(),
