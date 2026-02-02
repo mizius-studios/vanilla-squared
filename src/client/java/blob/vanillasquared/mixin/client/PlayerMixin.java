4dc4c2173d9295781e1f9d3c839141a218bd4b67
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-02 17:00:21 +0100

    Fishing Rod is finished!

---
+++ src/client/java/blob/vanillasquared/mixin/client/PlayerMixin.java
@@ -0,0 +1,33 @@
+package blob.vanillasquared.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MultiPlayerGameMode.class)
+public class PlayerMixin {
+
+    @Inject(method = "useItem(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"), cancellable = true)
+    private void vsq$prioritizeRod(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+
+        ItemStack mainhand = player.getMainHandItem();
+        if (hand == InteractionHand.MAIN_HAND && mainhand.getComponents().has(DataComponents.BLOCKS_ATTACKS)) {
+            ItemStack off = player.getOffhandItem();
+
+            if (off.getItem() instanceof FishingRodItem) {
+                cir.setReturnValue(InteractionResult.PASS);
+            }
+        }
+    }
+}
