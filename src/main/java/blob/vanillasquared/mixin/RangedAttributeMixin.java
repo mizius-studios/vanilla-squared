2f0f309c6d5b84aaa87b004585ef5a273446581d
pxlarified <jan.kramer@mizius.com>
2026-02-11 14:38:30 +0100

    sussy wussy beta testing

--- src/main/java/blob/vanillasquared/mixin/RangedAttributeMixin.java
+++ src/main/java/blob/vanillasquared/mixin/RangedAttributeMixin.java
@@ -1,22 +1,25 @@
 package blob.vanillasquared.mixin;
 
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RangedAttribute.class)
 public class RangedAttributeMixin {
-    @Inject(method = "<init>", at = @At("HEAD"))
-    private static void init(String string, double d, double e, double f, CallbackInfo ci) {
-        switch (string) {
-            case "attribute.name.armor": {
-                f = 1024.0;
-            }
-            case "attribute.name.armor_toughness": {
-                f = 1024.0;
-            }
+    @Shadow
+    @Final
+    @Mutable
+    private double maxValue;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void vsq$expandArmorRange(String descriptionId, double defaultValue, double minValue, double maxValue, CallbackInfo ci) {
+        if ("attribute.name.armor".equals(descriptionId) || "attribute.name.armor_toughness".equals(descriptionId)) {
+            this.maxValue = 1024.0;
         }
     }
 }
