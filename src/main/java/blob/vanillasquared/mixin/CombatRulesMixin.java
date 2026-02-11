4969bf7819e7070092ead0a9561991bc80570e48
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-11 16:14:52 +0100

    re-added BlobItems.java

--- src/main/java/blob/vanillasquared/mixin/CombatRulesMixin.java
+++ src/main/java/blob/vanillasquared/mixin/CombatRulesMixin.java
@@ -2,11 +2,13 @@ package blob.vanillasquared.mixin;
 
 import net.minecraft.world.damagesource.CombatRules;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(CombatRules.class)
 public class CombatRulesMixin {
+    @Unique
     private static final float VSQ_MAX_ARMOR_EFFECT = 1024.0F;
 
     @ModifyConstant(method = "getDamageAfterAbsorb", constant = @Constant(floatValue = 20.0F))
