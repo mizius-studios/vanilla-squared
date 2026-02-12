f91ffbfb5983d6fd89be7a98b2b3ec171846c378
pxlarified <jan.kramer@mizius.com>
2026-02-12 18:46:53 +0100

    asdsaddasd

--- src/main/java/blob/vanillasquared/mixin/LivingEntityAttributesMixin.java
+++ src/main/java/blob/vanillasquared/mixin/LivingEntityAttributesMixin.java
@@ -1,6 +1,5 @@
 package blob.vanillasquared.mixin;
 
-import blob.vanillasquared.VanillaSquared;
 import blob.vanillasquared.util.modules.attributes.RegisterAttributes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -14,8 +13,6 @@ public abstract class LivingEntityAttributesMixin {
 
     @Inject(method = "createLivingAttributes", at = @At("RETURN"))
     private static void vsq$addCustomAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
-        VanillaSquared.LOGGER.info("Adding maceProtection attribute to LivingEntity");
         cir.getReturnValue().add(RegisterAttributes.maceProtection);
-        VanillaSquared.LOGGER.info("Successfully added maceProtection attribute");
     }
 }
