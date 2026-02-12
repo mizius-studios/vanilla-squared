983b3209768ab4be9381c43b84960be7f90dc55a
pxlarified <jan.kramer@mizius.com>
2026-02-12 16:25:40 +0100

    asdsaddasd

---
+++ src/main/java/blob/vanillasquared/mixin/LivingEntityAttributesMixin.java
@@ -0,0 +1,18 @@
+package blob.vanillasquared.mixin;
+
+import blob.vanillasquared.util.modules.attributes.RegisterAttributes;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityAttributesMixin {
+
+    @Inject(method = "createLivingAttributes", at = @At("RETURN"))
+    private static void vsq$addCustomAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
+        cir.getReturnValue().add(RegisterAttributes.maceProtection);
+    }
+}
