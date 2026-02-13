7d35c40f3604a57869dfa169809b8ed16219e63a
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-13 16:32:02 +0100

    cleaned up AxeMixin.java added custom sword attributes

--- src/main/java/blob/vanillasquared/mixin/ToolMaterialMixin.java
+++ src/main/java/blob/vanillasquared/mixin/ToolMaterialMixin.java
@@ -1,16 +1,13 @@
 package blob.vanillasquared.mixin;
 
 import blob.vanillasquared.util.data.GeneralWeapon;
-import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.tags.BlockTags;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
-import net.minecraft.world.item.component.Tool;
-import net.minecraft.world.item.component.Weapon;
-import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
 import java.util.Map;
 
 @Debug(export = true)
@@ -36,13 +32,26 @@ public class ToolMaterialMixin {
             ToolMaterial.NETHERITE, new GeneralWeapon(8.0d, -2.4d, 0.0d, 2069)
     );
 
-    @Inject(method = "applySwordProperties", at = @At("HEAD"), cancellable = true)
-    private static void applySwordProperties(Item.Properties properties, float f, float g, CallbackInfoReturnable<Item.Properties> cir) {
-        cir.setReturnValue(this.applyCommonProperties(properties).component(DataComponents.TOOL, new Tool(List.of(Tool.Rule.minesAndDrops(HolderSet.direct(new Holder[]{Blocks.COBWEB.builtInRegistryHolder()}), 15.0F), Tool.Rule.overrideSpeed(holderGetter.getOrThrow(BlockTags.SWORD_INSTANTLY_MINES), Float.MAX_VALUE), Tool.Rule.overrideSpeed(holderGetter.getOrThrow(BlockTags.SWORD_EFFICIENT), 1.5F)), 1.0F, 2, false)).attributes(this.createSwordAttributes(f, g)).component(DataComponents.WEAPON, new Weapon(1)));
+    @Unique
+    private static final Identifier ATTRIBUTE_IDENTIFIER_REACH = Identifier.fromNamespaceAndPath("vanillasquared", "sword_reach");
 
-    }
     @Inject(method = "createSwordAttributes", at = @At("HEAD"), cancellable = true)
-    private static void createSwordAttributes(float f, float g, CallbackInfoReturnable<ItemAttributeModifiers> cir) {
+    private void createSwordAttributes(float f, float g, CallbackInfoReturnable<ItemAttributeModifiers> cir) {
+        ToolMaterial material = (ToolMaterial) (Object) this;
+        GeneralWeapon weapon = SWORD.getOrDefault(material, GeneralWeapon.DEFAULT);
 
+        double attackDamage = weapon.attackDamage();
+        double attackSpeed = weapon.attackSpeed();
+        double attackReach = weapon.entityReach();
+
+        ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder()
+                .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(Item.BASE_ATTACK_DAMAGE_ID, attackDamage, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
+                .add(Attributes.ATTACK_SPEED, new AttributeModifier(Item.BASE_ATTACK_SPEED_ID, attackSpeed, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
+
+        if (attackReach != 0.0d) {
+            builder.add(Attributes.ENTITY_INTERACTION_RANGE, new AttributeModifier(ATTRIBUTE_IDENTIFIER_REACH, attackReach, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
+        }
+
+        cir.setReturnValue(builder.build());
     }
 }
