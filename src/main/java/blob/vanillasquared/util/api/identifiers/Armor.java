8b87e822e810a486c3c9f3b194984f0b8322d4af
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-13 16:49:35 +0100

    added Armor.java added GeneralArmor.java

---
+++ src/main/java/blob/vanillasquared/util/api/identifiers/Armor.java
@@ -0,0 +1,8 @@
+package blob.vanillasquared.util.api.identifiers;
+
+import net.minecraft.world.item.equipment.ArmorMaterials;
+import net.minecraft.world.item.equipment.ArmorType;
+
+public class Armor {
+
+}
