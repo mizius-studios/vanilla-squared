3beda6f1478c0162b4d9018df5d202b276610b04
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-21 10:49:03 +0100

    progress on the API constructions

--- src/main/java/blob/vanillasquared/util/data/GeneralArmor.java
+++ src/main/java/blob/vanillasquared/util/data/GeneralArmor.java
@@ -8,15 +8,9 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import org.spongepowered.asm.mixin.Unique;
 
-public record GeneralArmor(
-    Identifier identifier,
-    double attributeArmor,
-    double attributeArmorToughness,
-    double attributeKnockbackResistance,
-    double attributeMaceProtection
-) {
-    public static ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder();
+public class GeneralArmor {
 
+    public static ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder();
     @Unique
     private static void vsq$armorModifier(ItemAttributeModifiers.Builder builder, Identifier id, double value, EquipmentSlotGroup slotGroup) {
         builder.add(Attributes.ARMOR, new AttributeModifier(id, value, AttributeModifier.Operation.ADD_VALUE), slotGroup);
@@ -35,5 +29,12 @@ public record GeneralArmor(
     @Unique
     private static void vsq$maceProtectionModifier(ItemAttributeModifiers.Builder builder, Identifier id, double value, EquipmentSlotGroup slotGroup) {
         builder.add(RegisterAttributes.maceProtection, new AttributeModifier(id, value, AttributeModifier.Operation.ADD_VALUE), slotGroup);
+    }
+
+    public GeneralArmor(Identifier identifier, EquipmentSlotGroup slotGroup, double attributeArmor, double attributeArmorToughness, double attributeKnockbackResistance, double attributeMaceProtection) {
+        vsq$armorModifier(builder, identifier, attributeArmor, slotGroup);
+        vsq$armorToughnessModifier(builder, identifier, attributeArmorToughness, slotGroup);
+        vsq$armorKnockbackResistanceModifier(builder, identifier, attributeKnockbackResistance, slotGroup);
+        vsq$maceProtectionModifier(builder, identifier, attributeMaceProtection, slotGroup);
     }
 }
