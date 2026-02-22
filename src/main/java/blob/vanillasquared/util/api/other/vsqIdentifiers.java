823d7c158f0fe327d844d1e30fce07475b103e4b
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-22 10:43:49 +0100

    - added .zed/ to the .gitignore files for setting purposes etc. - moved Identifier from AxeMixin to the API

--- src/main/java/blob/vanillasquared/util/api/other/vsqIdentifiers.java
+++ src/main/java/blob/vanillasquared/util/api/other/vsqIdentifiers.java
@@ -7,7 +7,8 @@ public enum vsqIdentifiers {
     vsqArmorChestplateOverride(Identifier.fromNamespaceAndPath("vanillasquared", "armor_chestplate_override")),
     vsqArmorLeggingsOverride(Identifier.fromNamespaceAndPath("vanillasquared", "armor_leggings_override")),
     vsqArmorBootsOverride(Identifier.fromNamespaceAndPath("vanillasquared", "armor_boots_override")),
-    vsqArmorHelmetOverride(Identifier.fromNamespaceAndPath("vanillasquared", "armor_helmet_override"));
+    vsqArmorHelmetOverride(Identifier.fromNamespaceAndPath("vanillasquared", "armor_helmet_override")),
+    vsqAxeReachOverride(Identifier.fromNamespaceAndPath("vanillasquared", "axe_reach"));
 
     private final Identifier identifier;
 
