3454feb99fb593c3ae1a7d8a72115b797f0facc6
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-02-26 14:21:17 +0100

    - added HitThrough DataComponent

---
+++ src/main/java/blob/vanillasquared/util/builder/components/HitThroughComponent.java
@@ -0,0 +1,35 @@
+package blob.vanillasquared.util.builder.components;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.Identifier;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Objects;
+
+public record HitThroughComponent(Identifier tag) {
+    public static final Codec<HitThroughComponent> CODEC = RecordCodecBuilder.create(instance ->
+            instance.group(
+                    Identifier.CODEC.fieldOf("tag").forGetter(HitThroughComponent::tag)
+            ).apply(instance, HitThroughComponent::new)
+    );
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, HitThroughComponent> STREAM_CODEC = StreamCodec.composite(
+            Identifier.STREAM_CODEC, HitThroughComponent::tag,
+            HitThroughComponent::new
+    );
+
+    public HitThroughComponent {
+        tag = Objects.requireNonNull(tag, "tag");
+    }
+
+    public boolean canHitThrough(BlockState blockState) {
+        TagKey<Block> blockTag = TagKey.create(Registries.BLOCK, this.tag);
+        return blockState.is(blockTag);
+    }
+}
---
+++ src/main/java/blob/vanillasquared/util/modules/components/DataComponents.java
@@ -0,0 +1,11 @@
+package blob.vanillasquared.util.modules.components;
+
+import blob.vanillasquared.util.builder.components.HitThroughComponent;
+import net.minecraft.core.component.DataComponentType;
+
+public final class DataComponents {
+    public static final DataComponentType<HitThroughComponent> HIT_THROUGH = RegisterComponents.hitThrough;
+
+    private DataComponents() {
+    }
+}
