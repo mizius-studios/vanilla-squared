ad25d6bf5d7e4920d807f6ae8e55200e2a95cddb
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-03-06 16:43:12 +0100

    .

--- src/client/java/blob/vanillasquared/main/VanillaSquaredClient.java
+++ src/client/java/blob/vanillasquared/main/VanillaSquaredClient.java
@@ -1,12 +1,9 @@
 package blob.vanillasquared.main;
-
-import blob.vanillasquared.main.gui.settings.controls.vsqControls;
 import net.fabricmc.api.ClientModInitializer;
 
 public class VanillaSquaredClient implements ClientModInitializer {
     public static final String MOD_ID = "vsq";
     @Override
     public void onInitializeClient() {
-        vsqControls.initialize();
     }
 }
