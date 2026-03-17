7cffec19dc9ea803aad4e6b25297a01a7065e8bb
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-03-17 20:12:00 +0100

    working on VSQEnchantmentScreen

--- src/client/java/blob/vanillasquared/main/gui/enchantment/VSQEnchantmentScreen.java
+++ src/client/java/blob/vanillasquared/main/gui/enchantment/VSQEnchantmentScreen.java
@@ -10,10 +10,10 @@ import net.minecraft.world.inventory.EnchantmentMenu;
 
 public class VSQEnchantmentScreen extends AbstractContainerScreen<EnchantmentMenu> {
     private static final Identifier GUI_TEXTURE = Identifier.fromNamespaceAndPath("vsq", "textures/gui/containers/enchantment_table.png");
-    private static final Identifier XP_ENABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "textures/gui/containers/enchantment_table/xp_requirement_enabled.png");
-    private static final Identifier XP_DISABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "textures/gui/sprites/containers/enchantment_table/xp_requirement_disabled.png");
-    private static final Identifier BLOCKS_ENABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "textures/gui/containers/enchantment_table/block_requirement_enabled.png");
-    private static final Identifier BLOCKS_DISABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "textures/gui/sprites/containers/enchantment_table/block_requirement_disabled.png");
+    private static final Identifier XP_ENABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "containers/enchantment_table/xp_requirement_enabled");
+    private static final Identifier XP_DISABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "containers/enchantment_table/xp_requirement_disabled");
+    private static final Identifier BLOCKS_ENABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "containers/enchantment_table/block_requirement_enabled");
+    private static final Identifier BLOCKS_DISABLED_SPRITE = Identifier.fromNamespaceAndPath("vsq", "containers/enchantment_table/block_requirement_disabled");
     private static final int TEX_W = 256;
     private static final int TEX_H = 256;
 
