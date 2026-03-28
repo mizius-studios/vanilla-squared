dce3e6a9cdfacb2cc07b65946526cec7c2a50d7f
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-03-28 13:52:32 +0100

    added preview for crafitng recipes

--- src/client/java/blob/vanillasquared/mixin/client/gui/GhostSlotsAccessor.java
+++ src/client/java/blob/vanillasquared/mixin/client/gui/GhostSlotsAccessor.java
@@ -1,14 +1,19 @@
 package blob.vanillasquared.mixin.client.gui;
 
+import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
 import net.minecraft.client.gui.screens.recipebook.GhostSlots;
 import net.minecraft.util.context.ContextMap;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.crafting.display.SlotDisplay;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(GhostSlots.class)
 public interface GhostSlotsAccessor {
     @Invoker("setInput")
     void vsq$setInput(Slot slot, ContextMap contextMap, SlotDisplay slotDisplay);
+
+    @Accessor("ingredients")
+    Reference2ObjectMap<Slot, Object> vsq$getIngredients();
 }
